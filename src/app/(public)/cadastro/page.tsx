ac1c637284da12c18ec7573a414fd6854b89f373
Mateus Prado <maateussp@gmail.com>
2026-03-19 11:49:08 -0300

    fix: correcao

---
+++ src/app/(public)/cadastro/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import RegiserForm from "./components/RegisterForm";
+
+
+export default function Regiser() {
+
+
+    
+    return (
+        <RegiserForm>
+            
+        </RegiserForm>
+    )
+
+}
