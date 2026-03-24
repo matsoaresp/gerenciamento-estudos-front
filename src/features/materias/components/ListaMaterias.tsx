c7d31121ed23930344497ff9e0d5d12291ad3afb
Mateus Prado <maateussp@gmail.com>
2026-03-24 10:49:02 -0300

    fix: correcao de exibicao dos formularios

--- src/features/materias/components/ListaMaterias.tsx
+++ src/features/materias/components/ListaMaterias.tsx
@@ -6,14 +6,14 @@ export type Materia = {
 
 type ListaMateriasProps = {
   materias: Materia[];
-  onCriarTopico: (materia: Materia) => void; 
+  onCriarTopico: () => void; 
 };
 
 export function ListaMaterias({ materias, onCriarTopico }: ListaMateriasProps) {
   return (
     <div className="w-full font-sans">
-      <div className="max-w-3xl mx-auto">
-        <h1 className="text-4xl font-black text-black mb-12 tracking-tight uppercase italic">
+      <div className="max-w-3xl mx-auto ">
+        <h1 className="flex text-4xl font-black text-black mb-12 tracking-tight uppercase italic items-center justify-center">
           / Minhas Matérias
         </h1>
 
@@ -37,21 +37,25 @@ export function ListaMaterias({ materias, onCriarTopico }: ListaMateriasProps) {
                   </p>
                 </div>
 
-                <button
-                  className="bg-[#332FD0] cursor-pointer text-white border-2 border-black px-6 py-3 font-bold uppercase tracking-widest shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:shadow-none hover:translate-x-[2px] hover:translate-y-[2px] transition-all active:bg-[#242193]"
-                  onClick={() => onCriarTopico(materia)} 
-                >
-                  Criar Tópico
-                </button>
+                
               </div>
             </div>
           ))}
+
+          <button
+                  className="bg-[#332FD0] cursor-pointer text-white border-2 border-black px-6 py-3 font-bold uppercase tracking-widest shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:shadow-none hover:translate-x-[2px] hover:translate-y-[2px] transition-all active:bg-[#242193]"
+                  onClick={() => onCriarTopico()} 
+                >
+                  Criar Matéria
+                </button>
 
           {materias.length === 0 && (
             <div className="border-4 border-dashed border-black rounded-lg p-12 text-center">
               <p className="text-xl font-bold uppercase">Nada por aqui ainda...</p>
             </div>
           )}
+
+  
         </div>
       </div>
     </div>
